dd166376e92118674dd2cfc21c045810b9fc25c0
sherrypan <sherrypann@outlook.com>
2015-08-31 13:40:45 +0800

    write person in prototype method

--- spec/practice_2/main_spec.js
+++ spec/practice_2/main_spec.js
@@ -3,11 +3,19 @@ var _ = require("lodash");
 var chai = require("chai");
 var sinon = require("sinon");
 var sinonChai = require("sinon-chai");
+var Person = require('../../src/practice_2.js').Person;
 var expect = chai.expect;
 chai.use(sinonChai);
 
-describe("OO入门", function(){
-    it("写一个类-2", function(){
-
+describe('Person',function(){
+    it("accept name,age as parameters",function(){
+        var person = new Person('pan',2);
+        expect(person.name).to.equal('pan');
+        expect(person.age).to.equal(2);
     });
-});
+    it("will introduce a person to you",function(){
+        var person = new Person('pan',2);
+        var introduce = person.introduce();
+        expect(introduce).to.equal("My name is pan. I am 2 years old.");
+    })
+})
