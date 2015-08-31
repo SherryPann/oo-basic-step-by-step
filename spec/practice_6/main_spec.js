54d64ee58fd3d228dce98e1a01ce3339da65e364
sherrypan <sherrypann@outlook.com>
2015-08-31 17:06:01 +0800

    rewrite the test for person ,student ,worker

--- spec/practice_6/main_spec.js
+++ spec/practice_6/main_spec.js
@@ -15,7 +15,7 @@ describe("Person", function(){
         expect(person.age).to.equal(11);
         expect(person.age).to.equal(11);
     });
-    it('return the correct introduce',function(){
+    it('should have a introduce method,introduce a person with name,age',function(){
         var person = new Person('haha',11);
         var introduce = person.introduce();
         expect(introduce).to.equal("My name is haha. I am 11 years old.")
@@ -28,7 +28,7 @@ describe('Student',function(){
         expect(student.age).to.equal(11);
         expect(student.klass).to.equal(2);
     });
-    it('return the correct introduce',function(){
+    it('overwrite person introduce ,introduce a student with name,age,klass',function(){
         var student = new Student('haha',11,2);
         var introduce = student.introduce();
         expect(introduce).to.equal("My name is haha. I am 11 years old.I am a Student. I am at Class 2.")
@@ -40,7 +40,7 @@ describe('Worker',function(){
         expect(worker.name).to.equal('haha');
         expect(worker.age).to.equal(11);
     });
-    it('return the correct introduce',function(){
+    it('overwrite person introduce ,introduce a worker with name,age',function(){
         var worker = new Worker('haha',11);
         var introduce = worker.introduce();
         expect(introduce).to.equal("My name is haha. I am 11 years old.I am a Worker. I have a job.")
